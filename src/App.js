3d958a6eeea5d73eea7eed693350549693074211
arbitraryrw <nickcucakovic@gmail.com>
2021-06-06 11:07:35 +0100

    Removing redundant boiler plate logo, updating required dependencies, and implementing components in main app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Deck from "./components/Deck";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="app">
+      <div className="container">
+        <div className="app-header">
+          <h1> Swipeable Cards Demo</h1>
+        </div>
+        <Deck/>
+      </div>
+    </div>    
+  )
 }
 
 export default App;
